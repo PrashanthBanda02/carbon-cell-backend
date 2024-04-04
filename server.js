0cff2d2dd19bc7673fd3c4d82d765b059a59ce7d
Prashanth_Banda <prashanthyadav22.py@gmail.com>
2024-04-04 12:28:55 +0530

    added new feature in register endpoint

--- server.js
+++ server.js
@@ -59,25 +59,37 @@ const User =  mongoose.model('User',{
 app.post('/register', async (req,res)=>{
     try{
         const { username, email, password } = req.body;
-
+        // const result = email.includes("@gmail.com")
+        
         // Check if the user already exists
-        const existingUser  = await User.findOne({email})
-        if(existingUser ){
-            return res.status(400).json({ message: 'User with this email already exists' });
+
+        const validEmail = email.includes('@gmail.com')
+
+        if (validEmail){
+            const existingUser  = await User.findOne({email})
+            if(existingUser ){
+                return res.status(400).json({ message: 'User with this email already exists' });
+            }
+
+            const hashedPassword = await bcrypt.hash(password,10)    // Hash the password
+
+            // Create a new user with hashed password
+            const newUser = new User({username,email, password: hashedPassword})
+
+            // Save the user to the database
+            await newUser.save()
+
+            // Send success response, 200 status code-new user resource has been created
+            res.status(201).json({ success: true, message: 'User registered successfully' });
+
+        }
+        else{
+            res.json({success: false, message:'Failed to register user' })
         }
 
-        const hashedPassword = await bcrypt.hash(password,10)    // Hash the password
+        }
 
-        // Create a new user with hashed password
-        const newUser = new User({username,email, password: hashedPassword})
-
-        // Save the user to the database
-        await newUser.save()
-
-        // Send success response, 200 status code-new user resource has been created
-        res.status(201).json({ success: true, message: 'User registered successfully' });
-
-    } catch(error){
+         catch(error){
         // Send error response, status code - 500 Internal Server Error
         res.status(500).json({success: false, message:'Failed to register user', error: error.message })
 
@@ -90,7 +102,6 @@ app.post('/register', async (req,res)=>{
 app.get('/protected', verifyToken, (req, res) => {
     res.json({ success: true, message: 'Authenticated user.' });
 });
-
 // API Endpoint for user login
 
 app.post('/login', async (req, res) => {
